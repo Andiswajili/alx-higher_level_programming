d5c8859a471a26e30a5592e09b8b49c6c7492cd1
Andiswajili <andiswajili01@gmail.com>
2024-01-04 04:07:20 -0800

    Updated

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -4,4 +4,13 @@ def magic_calculation(a, b, c):
         return (c)
     if c > b:
         return (a + b)
-    return (a*b - c)
+    return (a*b - c#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+        for n in range(4, 6):
+            c = add(c, n)
+        return c
+    else:
+        return sub(a, b))
